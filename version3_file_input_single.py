39729d244eee2e8685c9554e16aad9efeb1f0a0f
vutukuruyeshwanth <yeshwanthvutukuru31@gmail.com>
2025-08-18 01:31:33 +0530

    Create version3_file_input_single.py

---
+++ version3_file_input_single.py
@@ -0,0 +1,11 @@
+with open("inputs_single.txt", "r") as f:
+    lines = f.readlines()
+
+a = float(lines[0])
+b = float(lines[1])
+c = float(lines[2])
+t = float(lines[3])
+
+T = a * t**2 + b * t + c
+print(f"Predicted temperature at t={t}: {T:.2f}°C")
+
