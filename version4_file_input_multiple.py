72160b7740f613786b1449ea037daba10085b91d
vutukuruyeshwanth <yeshwanthvutukuru31@gmail.com>
2025-08-18 01:34:16 +0530

    Create version4_file_input_multiple.py

---
+++ version4_file_input_multiple.py
@@ -0,0 +1,5 @@
+with open("inputs_multiple.txt", "r") as f:
+    for line in f:
+        a, b, c, t = map(float, line.strip().split())
+        T = a * t**2 + b * t + c
+        print(f"a={a}, b={b}, c={c}, t={t} -> T={T:.2f}°C")
